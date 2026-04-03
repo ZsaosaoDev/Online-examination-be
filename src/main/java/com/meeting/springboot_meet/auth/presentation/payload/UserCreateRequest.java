0db46605fa521f37d3b16dbf6cd7213d6b64f8d6
Nguyen Huy Vu <huyvu10012003@gmail.com>
2026-04-03 18:44:47 +0700

    Task login

--- src/main/java/com/meeting/springboot_meet/auth/presentation/payload/UserCreateRequest.java
+++ src/main/java/com/meeting/springboot_meet/auth/presentation/payload/UserCreateRequest.java
@@ -1,5 +1,6 @@
 package com.meeting.springboot_meet.auth.presentation.payload;
 
+import com.meeting.springboot_meet.auth.domain.model.UserRole;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -8,13 +9,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class RegisterRequest {
+public class UserCreateRequest {
     @NotBlank(message = "Email không được để trống")
     @Email(message = "Định dạng email không hợp lệ")
     private String email;
@@ -22,4 +24,7 @@ public class RegisterRequest {
     @NotBlank(message = "Mật khẩu không được để trống")
     @Size(min = 6, message = "Mật khẩu phải có ít nhất 6 ký tự")
     private String password;
+
+    @Builder.Default
+    private Set<UserRole> roles = new HashSet<>();
 }
