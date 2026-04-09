cfb9aa9a78653fc7e59cf7bc8e53e0a719099ada
Nguyen Huy Vu <huyvu10012003@gmail.com>
2026-04-09 18:33:20 +0700

    fix sec

--- src/main/java/com/meeting/springboot_meet/security/config/SecurityConfig.java
+++ src/main/java/com/meeting/springboot_meet/security/config/SecurityConfig.java
@@ -79,11 +79,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOriginPatterns(List.of("*"));
-        configuration.setAllowedOrigins(List.of(
-                "http://localhost:5173",
-                "http://localhost:3000"
-        ));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
